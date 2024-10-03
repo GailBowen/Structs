483b6a5cd91c27712ce9a4622a28c98f6ef60ba3
GailFoad <gail.foad@thenavpeople.co.uk>
2024-10-03 12:42:39 +0100

    embedded structs

--- main.go
+++ main.go
@@ -5,12 +5,25 @@ import "fmt"
 type person struct {
 	firstname string
 	lastname  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email    string
+	houseNum int
 }
 
 func main() {
 	fmt.Println("hello structs")
 
-	bob := person{firstname: "Gail", lastname: "Foad"}
+	bob := person{
+		firstname: "Gail",
+		lastname:  "Foad",
+		contact: contactInfo{
+			email:    "bob@gmail.com",
+			houseNum: 76,
+		},
+	}
 
 	// fmt.Println(bob)
 
