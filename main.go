c49b23390b2484d149328bf05d9c8ff33bed74f6
GailFoad <gail.foad@thenavpeople.co.uk>
2024-10-03 12:16:47 +0100

    type person struct

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+type person struct {
+	firstname string
+	lastname  string
+}
+
 func main() {
 	fmt.Println("hello structs")
+
+	bob := person{firstname: "Gail", lastname: "Foad"}
+
+	// fmt.Println(bob)
+
+	var Romanov person
+	Romanov.firstname = "Nicolas"
+
+	fmt.Println(Romanov.firstname)
+	fmt.Printf("%+v", bob)
+
 }
