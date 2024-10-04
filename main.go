eea4713e647a53d4dde469306141c98d41a0e26d
GailFoad <gail.foad@thenavpeople.co.uk>
2024-10-04 16:02:10 +0100

    more map

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type person struct {
 	firstname string
@@ -15,25 +17,31 @@ type contactInfo struct {
 
 func main() {
 
-	// var colours map[string]string
+	colours := map[string]string{
+		"purple": "#800080",
+		"green":  "#00FF00",
+		"brown":  "#964B00",
+	}
 
-	// colours := map[string]string{
-	// 	"purple": "#800080",
-	// 	"green":  "#00FF00",
-	// }
+	printMap(colours)
 
-	//fmt.Println(colours)
+	fmt.Printf("%v+", colours)
 
-	colours := make(map[string]string)
-	colours["purple"] = "#800080"
-	colours["green"] = "#00FF00"
+	sl := []int{3, 7, 2}
 
-	fmt.Printf("%+v\n", colours)
+	for i := 0; i < len(sl); i++ {
+		fmt.Println(sl[i])
+	}
 
-	delete(colours, "purple")
+}
 
-	fmt.Printf("%+v\n", colours)
+func printMap(c map[string]string) {
 
+	for colour, hex := range c {
+		fmt.Println(colour, hex)
+	}
+
+	c["silver"] = "#C0C0C0"
 }
 
 func byVal(q *string) {
