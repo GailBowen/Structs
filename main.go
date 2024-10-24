dc9249f96162e183077cd180dd59e629d12162e0
GailFoad <gail.foad@thenavpeople.co.uk>
2024-10-24 17:49:10 +0100

    io.Copy()

--- main.go
+++ main.go
@@ -9,23 +9,13 @@ import (
 
 func main() {
 
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/2")
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/3")
 
 	if err != nil {
 		log.Fatalln(err)
 		os.Exit(1)
 	}
 
-	//fmt.Println(resp)
-
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Printf(string(body))
+	io.Copy(os.Stdout, resp.Body)
 
 }
